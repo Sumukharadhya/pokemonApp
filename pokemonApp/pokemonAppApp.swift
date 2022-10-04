781852bbb01d0c37c9d8f98db6bf367cf00ee16d
Sumukh <sumukharadhyakj07@gmail.com>
2022-10-04 16:50:09 +0530

    Initial Commit

---
+++ pokemonApp/pokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonAppApp.swift
+//  pokemonApp
+//
+//  Created by Sumukh Kj on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct pokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
